804364a08285ec80796594f7178deda532310d48
omerfbolat <anekdo1@gmail.com>
2024-05-24 16:16:30 +0300

    new theme and all colours added

---
+++ lib/utils/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color.fromRGBO(255, 118, 34, 1);
+Color primaryLight = const Color.fromRGBO(255, 198, 174, 1);
+Color primaryLight2 = const Color.fromRGBO(255, 225, 206, 1);
+Color infoLight = const Color.fromRGBO(240, 245, 250, 1); // input background
+Color grayscale = const Color.fromRGBO(156, 155, 166, 1);
+Color grayscaleLight = const Color.fromRGBO(236, 240, 244, 1);
+Color error = const Color.fromRGBO(224, 68, 68, 1);
+Color black = const Color.fromRGBO(0, 0, 0, 1);
+Color white = const Color.fromRGBO(255, 255, 255, 1);
+Color font = const Color.fromRGBO(100, 105, 130, 1);
+Color fontBold = const Color.fromRGBO(50, 52, 62, 1);
+Color fontPlaceholder = const Color.fromRGBO(160, 165, 186, 1);
+Color fontError = const Color.fromRGBO(255, 51, 38, 1);
+Color border = const Color.fromRGBO(237, 237, 237, 1);
+Color emptyImage = const Color.fromRGBO(152, 168, 184, 1);
+Color background = const Color.fromRGBO(18, 18, 35, 1);
